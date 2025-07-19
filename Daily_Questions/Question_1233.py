f92180dd1f718a89d3ea3a32fc43f15de529a9ab
VedantPatel <vedantpatel1969@gmail.com>
2025-07-19 10:24:27 +0530

    Daily_Question_Solved

---
+++ Daily_Questions/Question_1233.py
@@ -0,0 +1,10 @@
+#Remove Sub-Folders from the Filesytem
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        result=[]
+        
+        for path in folder:
+            if not result or not path.startswith(result[-1] + '/'):
+                result.append(path)
+        return result
